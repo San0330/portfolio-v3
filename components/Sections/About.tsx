8e52b099817a4661a1e79c44d746ead9b5621051
San0330 <sanneupane1024@gmail.com>
2022-09-24 12:40:49 +0545

    removed unused font-awesome link

--- components/Sections/About.tsx
+++ components/Sections/About.tsx
@@ -1,6 +1,7 @@
 import IntroParagraph from "./UIs/IntroParagraph";
 import Section from "./Section";
 import { Heading1, Heading2 } from "./UIs/Headings";
+import { AiOutlineArrowDown } from 'react-icons/ai'
 
 const About = () => {
     return (
@@ -25,11 +26,11 @@ const About = () => {
 
 const ResumeDownloadButton = () =>
     <button
-        className='bg-white shadow-xl mt-5 hover:ring-1 text-gray-600 border border-gray-300 inline-flex justify-center gap-2 w-36 text-center rounded-lg px-5 py-3 cursor-pointer uppercase tracking-widest'
+        className='bg-white shadow-xl mt-5 hover:ring-1 text-gray-600 border border-gray-300 inline-flex justify-center items-center gap-2 w-36 text-center rounded-lg px-5 py-3 cursor-pointer uppercase tracking-widest'
         onClick={() => window.open('/files/Santosh_Neupane_Resume.pdf', '_blank')}>
 
         <div className="animate-custom-bounce">
-            <i className="fa fa-arrow-down"></i>
+            <AiOutlineArrowDown size={20} />
         </div>
 
         Resume
--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
     return (
         <Html lang='en'>
             <Head>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-
                 <link rel='icon' type='image/png' href='/favicon-32x32.png' sizes="32x32" />
                 <link rel='icon' type='image/png' href='/favicon-16x16.png' sizes="16x16" />
 
