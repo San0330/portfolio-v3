84409793f75303327b09431feb1c433933afb797
San0330 <sanneupane1024@gmail.com>
2022-09-24 19:30:33 +0545

    reset active offset for sidedrawer nav

--- components/Sidebar/Nav/NavItem.tsx
+++ components/Sidebar/Nav/NavItem.tsx
@@ -15,7 +15,7 @@ const NavItem = ({ link, title }: NavItemProp) => {
     return (
         <li className={`${Style.navLink} cursor-pointer`} >
             <Scroll.Link
-                offset={-280}
+                offset={-50}
                 to={link}
                 activeClass={Style.active}
                 spy={true}
