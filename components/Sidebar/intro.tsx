bb9f65b4fe8e296a4711e3d4025324da3b3e9133
San0330 <sanneupane1024@gmail.com>
2022-09-25 01:18:00 +0545

    responsive sidebar intro for sm screen

--- components/Sidebar/intro.tsx
+++ components/Sidebar/intro.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 function Intro() {
     return (
-        <>
-            <div className="w-36 h-36 relative overflow-hidden ring-1 hover:ring-2 ring-gray-300 mb-5 rounded-full border-5 border-red-500 mx-auto">
+        <div className='flex flex-row md:flex-col px-3 items-center gap-x-5'>
+            <div className="w-16 h-16 md:w-36 md:h-36 relative overflow-hidden ring-1 hover:ring-2 ring-gray-300 md:mb-5 rounded-full border-5 border-red-500 md:mx-auto">
                 <Image
                     src="/images/me.jpg"
                     layout="fill"
@@ -11,18 +11,18 @@ function Intro() {
                     className='absolute object-fill aspect-square' />
             </div>
             <div className="text-center">
-                <h1 className='font-bold text-xl tracking-wide'>
+                <h1 className='font-bold text-base md:text-xl tracking-wide'>
                     <a href="#">
                         Santosh Neupane
                     </a>
                 </h1>
-                <span className="position">
+                <span className="position text-sm md:text-base text-gray-600">
                     <a href="#">
                         Software Engineer
                     </a>
                 </span>
             </div>
-        </>
+        </div>
     )
 }
 
