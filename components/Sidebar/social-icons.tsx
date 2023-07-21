33329616636cd770b7d5659038fcb087e3963cad
Santosh <sanneupane1024@gmail.com>
2023-07-21 16:11:28 +0545

    added linkedin follow button

--- components/Sidebar/social-icons.tsx
+++ components/Sidebar/social-icons.tsx
@@ -5,39 +5,43 @@ const SocialIcons = () => {
     const linkedInLink = 'https://www.linkedin.com/in/santosh-neupane-0330/'
     const githubLink = 'https://github.com/San0330'
     const mail = 'santosh.neupane.0330@gmail.com'
+    const linkedFollowLink = 'https://www.linkedin.com/comm/mynetwork/discovery-see-all?usecase=PEOPLE_FOLLOWS&followMember=santosh-neupane-0330'
 
     const openMail = () => window.open(`mailto:${mail}`)
 
     return (
-        <div className="flex justify-around items-end w-full my-4">
-            <div
-                className={`relative cursor-pointer ${Style.mailIcon}`}
-                onClick={openMail}
-            >
-                <AiOutlineMail size={25} color="gray" />
+        <div>
+            <a className={`flex mx-auto ${Style.libutton}`} href={linkedFollowLink} target="_blank">Follow</a>
+            <div className="flex justify-around items-end w-full my-4">
                 <div
-                    className={`bg-black/50 text-sm z-20 text-white text-md rounded-md px-4 py-2 top-8 -left-8 absolute ${Style.tooltip}`}
+                    className={`relative cursor-pointer ${Style.mailIcon}`}
+                    onClick={openMail}
                 >
-                    {mail}
+                    <AiOutlineMail size={25} color="gray" />
+                    <div
+                        className={`bg-black/50 text-sm z-20 text-white text-md rounded-md px-4 py-2 top-8 -left-8 absolute ${Style.tooltip}`}
+                    >
+                        {mail}
+                    </div>
                 </div>
-            </div>
-            <div>
-                <a
-                    href={linkedInLink}
-                    rel="noreferrer noopener"
-                    target="_blank"
-                >
-                    <AiFillLinkedin size={25} color="blue" />
-                </a>
-            </div>
-            <div>
-                <a
-                    href={githubLink}
-                    rel="noreferrer noopener"
-                    target="_blank"
-                >
-                    <AiFillGithub size={25} color='gray' />
-                </a>
+                <div>
+                    <a
+                        href={linkedInLink}
+                        rel="noreferrer noopener"
+                        target="_blank"
+                    >
+                        <AiFillLinkedin size={25} color="blue" />
+                    </a>
+                </div>
+                <div>
+                    <a
+                        href={githubLink}
+                        rel="noreferrer noopener"
+                        target="_blank"
+                    >
+                        <AiFillGithub size={25} color='gray' />
+                    </a>
+                </div>
             </div>
         </div>
     )
