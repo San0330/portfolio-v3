57bcf3e4137567c58315705f84f7ec760041cdf1
Santosh Neupane <40764689+San0330@users.noreply.github.com>
2024-02-10 22:12:40 +0545

    hide some more certs

--- components/Sections/Achievements.tsx
+++ components/Sections/Achievements.tsx
@@ -38,12 +38,12 @@ const AchievementsCarousels = () => {
                 <Carousel image='/images/certs/problem_solving.png' />
                 <Carousel image='/images/certs/icpc.jpg' />
                 <Carousel image='/images/certs/flutter.jpg' />
-                <Carousel image='/images/certs/fundamental_analysis.png' />
-                <Carousel image='/images/certs/python.png' />
+                <Carousel image='/images/certs/fundamental_analysis.png' />               
                 <Carousel image='/images/certs/web_certificate.jpg' />
+                {/* <Carousel image='/images/certs/python.png' /> */}
                 {/* <Carousel image='/images/certs/pokhara_prog_contest.jpg' /> */}
                 {/* <Carousel image='/images/certs/hackathon.jpg' /> */}
-                <Carousel image='/images/certs/nepali_samaj.jpg' />
+                {/* <Carousel image='/images/certs/nepali_samaj.jpg' /> */}
             </Slider>
         </div>
     )
