8e61e287f2477afc5c6c2c72164f67933bc131d8
Santosh Neupane <40764689+San0330@users.noreply.github.com>
2024-07-26 14:13:40 +0545

    minor update

--- app/components/Sections/Blogs.tsx
+++ app/components/Sections/Blogs.tsx
@@ -8,7 +8,7 @@ const Blogs = () => {
             <Heading1 title="sharing knowledge" />
             <Heading2 title='Blogs' />
             <section className=" flex flex-col gap-10">
-                <p className="block text-gray-400">Coming Soon!</p>
+                <p className="block text-gray-400 text-2xl">Coming Soon!</p>
                 <p className="block text-gray-400">Demo implentation on "blog" branch of github repository</p>
             </section>
         </Section>
