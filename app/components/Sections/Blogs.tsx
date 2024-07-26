0138dc16662ca68045b0a430f3ccb6a039e48752
Santosh Neupane <40764689+San0330@users.noreply.github.com>
2024-07-26 14:13:01 +0545

    minor update

--- app/components/Sections/Blogs.tsx
+++ app/components/Sections/Blogs.tsx
@@ -2,13 +2,14 @@
 import Section from "./Section";
 import { Heading1, Heading2 } from "./UIs/Headings";
 
-const Blogs = () => {   
+const Blogs = () => {
     return (
         <Section name='blogs'>
             <Heading1 title="sharing knowledge" />
             <Heading2 title='Blogs' />
-            <section className="md:grid md:grid-cols-2 flex flex-col gap-10">
-                Coming Soon...
+            <section className=" flex flex-col gap-10">
+                <p className="block text-gray-400">Coming Soon!</p>
+                <p className="block text-gray-400">Demo implentation on "blog" branch of github repository</p>
             </section>
         </Section>
     )
